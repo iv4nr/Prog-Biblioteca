d8b1d87b4cc9b7bb8514d6c0f7e83c09252d4ece
Iván Rodríguez <ivanruiz@myuax.com>
2026-03-02 11:58:25 +0100

    feat (Libro): Creada la clase libro

--- src/Libro.java
+++ src/Libro.java
@@ -1,3 +1,62 @@
 public class Libro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private int numPaginas;
 
+
+    /**
+     * Metodo constructor ded Libros
+     * @param id identificador del libro
+     * @param titulo titulo del libro
+     * @param autor autor del libro
+     * @param numPaginas numero de paginas del libro
+     */
+    public Libro(int id, String titulo, String autor, int numPaginas) {
+        int numHojas; // variable del metodo
+
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numPaginas = numPaginas;
+
+        numHojas = numPaginas/2;
+    }
+
+    /**
+     * Devuelve el id del libro
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Devuelve el titulo del libro
+     * @return titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Libro: {");
+        sb.append(String.format(" id: %d;", id));
+        sb.append(String.format(" titulo: %s;", titulo));
+        sb.append(String.format(" autor: %s;",  autor));
+        sb.append(String.format(" numPaginas: %d;",  numPaginas));
+        sb.append("}");
+
+        return sb.toString();
+    }
 }
