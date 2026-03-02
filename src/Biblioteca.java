d112789fcbe5149db36add55afba1f903850e5a1
Iván Rodríguez <ivanruiz@myuax.com>
2026-03-02 11:59:12 +0100

    feat (Biblioteca): Creada la clase Biblioteca con constructor y el metodo findLibro

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,2 +1,36 @@
 public class Biblioteca {
+
+    private Libro[] catalogo;
+    private final int CANT_MAX_LIBROS;
+
+
+    public Biblioteca(int cantidad_maxima_libros) {
+        CANT_MAX_LIBROS = cantidad_maxima_libros;
+        catalogo = new Libro[CANT_MAX_LIBROS];
+
+    }
+
+
+    /**
+     * Busca el libro en el catalogo por id
+     * @param id_libro id del libro buscado
+     * @return la posicion del libro
+     */
+    public Libro findLibro(int id_libro){
+        boolean seguirBuscando = true;
+        Libro libroEncontrado = null;
+
+        for(int i = 0; i < CANT_MAX_LIBROS && seguirBuscando; i++){
+            if( catalogo[i] != null && catalogo[i].getId() == id_libro){
+                libroEncontrado = catalogo[i];
+                seguirBuscando = false;
+            }
+        }
+
+        return libroEncontrado;
+    }
+
+
+
+
 }
