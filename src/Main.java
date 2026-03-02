de434d880567fc1d90fc6b2ad9ff27759882c258
Iván Rodríguez <ivanruiz@myuax.com>
2026-03-02 12:47:01 +0100

    feat(Main): Pruebas con el Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,18 @@
 public class Main {
 
+    public static void main(String[] args) {
+
+        Biblioteca biblioteca = new Biblioteca(10);
+        Libro libro1 = new Libro(1,"El Quijote","Miguel de Cervantes",800);
+
+        Libro libro2 = new Libro(2,"El Gato con Botas", "Charles Perrault",100);
+
+        System.out.println(biblioteca.findLibro(1));
+
+        System.out.println(biblioteca.añadirLibro(libro1));
+        System.out.println(biblioteca.añadirLibro(libro2));
+
+        System.out.println(biblioteca.obtenerCatalogo());
+
+    }
 }
